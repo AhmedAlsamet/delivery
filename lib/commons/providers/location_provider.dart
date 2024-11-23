6b32cd63b7dc5d62f00e6456e086949e279bdf68
AhmedAlsamet <abutheebrefal@gmail.com>
2024-11-23 04:34:56 +0300

    location error

--- lib/commons/providers/location_provider.dart
+++ lib/commons/providers/location_provider.dart
@@ -13,19 +13,51 @@ class LocationProvider with ChangeNotifier {
 
   Placemark get address => _address;
   Position _currentLocation = Position(
-    latitude: 0, longitude: 0,
-    speed: 1, speedAccuracy: 1, altitude: 1, accuracy: 1, heading: 1, timestamp: DateTime.now(),
-    altitudeAccuracy: 1, headingAccuracy: 1,
+    latitude: 0,
+    longitude: 0,
+    speed: 1,
+    speedAccuracy: 1,
+    altitude: 1,
+    accuracy: 1,
+    heading: 1,
+    timestamp: DateTime.now(),
+    altitudeAccuracy: 1,
+    headingAccuracy: 1,
   );
   Position get currentLocation => _currentLocation;
 
   Future<Position> locateUser() async {
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // SHOW MESSAGE;
+      return _currentLocation;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // SHOW MESSAGE;
+        return _currentLocation;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // SHOW MESSAGE;
+      return _currentLocation;
+    }
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
   }
 
   void getUserLocation() async {
     _currentLocation = await locateUser();
-    var currentAddresses = await placemarkFromCoordinates(_currentLocation.latitude, _currentLocation.longitude);
+    var currentAddresses = await placemarkFromCoordinates(
+        _currentLocation.latitude, _currentLocation.longitude);
     _address = currentAddresses.first;
     notifyListeners();
   }
